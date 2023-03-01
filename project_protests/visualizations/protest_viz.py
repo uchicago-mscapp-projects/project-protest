ae33c3a1475934373f557da1fa9651a05be7d1fd
Lisette Solis <lisettesolis@linux7.cs.uchicago.edu>
2023-03-01 00:27:57 -0600

    Month graph

--- project_protests/visualizations/protest_viz.py
+++ project_protests/visualizations/protest_viz.py
@@ -3,29 +3,18 @@ import plotly.express as px
 
 def protest_data():
     # change to the create path using parent wd so it's not specific to me 
-<<<<<<< HEAD
-    df = pd.DataFrame(pd.read_csv("/home/lisettesolis/30122-project-project-protest/project_protests/count_data/test_police-data.csv",on_bad_lines='skip'), columns = ['Location', 'Date', 'County', 'StateTerritory', \
-        'City_Town'])
-    # df = pd.DataFrame(pd.read_csv("/home/lisettesolis/30122-project-project-protest/project_protests/visualizations/test_police-data.csv",on_bad_lines='skip'), columns = ['Location', 'Date', 'County', 'StateTerritory', \
-    #     'City_Town'])
-    df['Year'] = df['Date'].str.split('/', 2).str[2]
-
-    return df.head()
-=======
-    df = pd.DataFrame(pd.read_csv("/home/lisettesolis/30122-project-project-protest/project_protests/visualizations/test_police-data.csv",on_bad_lines='skip'), columns = ['Location', 'Date', 'County', 'StateTerritory', \
-        'City_Town'])
-    df['Year'] = df['Date'].str.split('/', 2).str[2]
-
+    df = pd.DataFrame(pd.read_csv("/home/lisettesolis/30122-project-project-protest/project_protests/count_data/police-data.csv",on_bad_lines='skip'),\
+        columns = ['Location', 'Date', 'County', 'StateTerritory','City_Town'])
+    df['Date']= pd.to_datetime(df['Date'])
+    df['Month'] = df['Date'].dt.month
+    df['Year'] = df['Date'].dt.year 
+    
     return df
->>>>>>> main
 
 def city_counts():
     df = protest_data()
     df_pivot = pd.pivot_table(df, index='Year', values='Date',
                           aggfunc='count', margins=True, margins_name='Count')
-<<<<<<< HEAD
-    return df_pivot
-=======
     # df.groupby(['County','Year']).size().to_frame()
     return df_pivot
 
@@ -43,4 +32,3 @@ def city_counts():
 
 
     # return df_grouped.head()
->>>>>>> main
