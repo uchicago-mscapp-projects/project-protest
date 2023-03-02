a12751329193f5e1ad20e8bedf625aab7e2f1094
Monica Nimmagadda <monican@linux6.cs.uchicago.edu>
2023-03-01 22:30:24 -0600

    histograms by year

--- project_protests/project_protests/sentiment_analysis/pairwise_correlation.py
+++ project_protests/project_protests/sentiment_analysis/pairwise_correlation.py
@@ -1,47 +1,50 @@
 import pandas as pandas
 import re  
 import nltk  
-# nltk.download('stopwords')
 from nltk.corpus import stopwords  
 import numpy as np
 from gensim.models import Word2Vec  
 import pandas as pd
+import plotly.express as px
 from nltk.stem.porter import *  
+import statistics
 stemmer = PorterStemmer()  
-# nltk.download('punkt')
+
 # Monica # 
 
-def word_similarity():
+def word_similarity(term):
     # upload text
     df = pd.read_csv('/home/monican/capp30122/30122-project-project-protest/project_protests/clean_data/raw_data/nyt_articles.csv')
-    fake_text = df['lead_paragraph'].str.cat(sep=' ')
-    # for row in df.iterrows()
-    # fake_text = df.iloc[500]['lead_paragraph']
-    fake_text = fake_text.lower()
-    # fake_text_split = fake_text.split()
-    
+    df['year'] = pd.DatetimeIndex(df['date']).year
+    years = df['year'].unique()
 
-    # clean text
-    # processed_text = fake_text_split.lower()
-    # print(fake_text_split)
-    processed_text = re.sub('[^a-zA-Z]', ' ', fake_text)
-    processed_text = re.sub(r'\s+', ' ', processed_text)
-    # print("PROCESSED TEXT: ", processed_text)
-    # processed_text = processed_text.split()
-    sentences = nltk.sent_tokenize(processed_text)
-    # print("SENTENCES: ", sentences)
+    for year in years:
+        articles_text = df.loc[df['year']==year]['lead_paragraph'].str.cat(sep=' ')
+        text = clean(articles_text)
+        # mode = statistics.mode(text[0])
+        word2vec = Word2Vec(text, min_count = 10)
+        similar_words = word2vec.wv.most_similar(positive=[term], topn=30)
+        visualize_simiilarity(similar_words, year)
+    return None
+
+def clean(text):
+    text = text.lower()
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    # splits a paragraph into sentences
+    sentences = nltk.sent_tokenize(text)
+    # splits a sentence into words
     words = [nltk.word_tokenize(sent) for sent in sentences]
-    # print("WORDS: ", words)
+    # removes stop words
     for i in range(len(words)):
         words[i] = [w for w in words[i] if w not in stopwords.words('english')]
+    return  words
 
-    # print(words)
-    word2vec = Word2Vec(words, min_count = 100)
-    # vocab = word2vec.wv.index_to_key
-    # print("VOCAB: ", vocab)
-
-    # v1 = word2vec.wv['legal']
-    sim_words = word2vec.wv.most_similar('police')
-    print(sim_words)
+def visualize_simiilarity(similar_words, year):
+    title = str(year)
+    df = pd.DataFrame(similar_words, columns=["word", "score"])
+    hist = px.histogram(df, x="word", y="score",title=title)
+    hist.update_layout(yaxis_range=[0.0, 1.0])
+    # hist.update_layout(title_x: year)
+    hist.show()
     return None
-# model = word2vec.Word2Vec(fake_text_split, vector_size=100, window=5, min_count=10, workers=4)  
