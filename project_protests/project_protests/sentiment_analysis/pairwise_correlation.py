c830b101f87390dd08d11d05cb1ce27aa8d875e6
Monica Nimmagadda <monican@linux6.cs.uchicago.edu>
2023-03-04 16:37:33 -0600

    hopefully this works

--- project_protests/project_protests/sentiment_analysis/pairwise_correlation.py
+++ project_protests/project_protests/sentiment_analysis/pairwise_correlation.py
@@ -6,36 +6,53 @@ import numpy as np
 from gensim.models import Word2Vec  
 import pandas as pd
 import plotly.express as px
-from nltk.stem.porter import *  
-import statistics
-stemmer = PorterStemmer()  
-
-# Monica # 
+from plotly.subplots import make_subplots
+import pathlib
+# Monica Nimmagadda # 
 
 def word_similarity(term):
-    # upload text
-    df = pd.read_csv('/home/monican/capp30122/30122-project-project-protest/project_protests/project_protests/api_requests/the_guardian/data/the_guardian_compiled.csv')
-    df_nyt = pd.read_csv('/home/monican/capp30122/30122-project-project-protest/project_protests/clean_data/raw_data/nyt_articles.csv')
+    '''
+    This function reads news data, calls the clean function, and creates a 
+    Word2Vec model for each year of data as it relates to police and 
+    Black Lives Matter. The function then creates a visualization of the combined
+    data.
+
+    Input: term (string) - the term we are looking for similarity
+    Output: 2x3 chart with top 10 words per year and their corresponding
+    similarity score
+    '''
+    # parent = 
+    # os.path.join(parent, directory) 
+    nyt_filepath = pathlib.Path(__file__).parent.parent.parent / "clean_data/raw_data/nyt_articles.csv"
+    df = pd.read_csv(nyt_filepath)
+    guardian_filepath = pathlib.Path(__file__).parent.parent / "api_requests/the_guardian/data/the_guardian_compiled.csv"
+    df_nyt = pd.read_csv(guardian_filepath)
     df['year'] = pd.DatetimeIndex(df['date']).year
     df_nyt['year'] = pd.DatetimeIndex(df_nyt['date']).year
     df.merge(df_nyt[['year', 'lead_paragraph']])
-
     years = sorted(df['year'].unique())
     visualization_df = []
     for year in years:
         articles_text = df.loc[df['year']==year]['lead_paragraph'].str.cat(sep=' ')
         text = clean(articles_text)
         if year == 2017:
-            word2vec = Word2Vec(text, min_count = 1, sg=1, window=7)
+            word2vec = Word2Vec(text, min_count = 3, sg=1, window=7)
         elif year == 2023:
             continue
         else:
             word2vec = Word2Vec(text, min_count = 10, sg=1, window=7)
         similar_words = word2vec.wv.most_similar(positive=[term], topn=10)
-        visualize_simiilarity(similar_words, year)
-    return None
+        for word, score in similar_words:
+            visualization_df.append((word, score, year))
+    visualize_simiilarity(visualization_df)
+    return visualization_df
 
 def clean(text):
+    '''
+    This function cleans the leading paragraph text from the news articles passed in.
+    It also removes english stop words as defined by NLTK and adds in common
+    meaningless words.
+    '''
     text = text.lower()
     text = re.sub('[^a-zA-Z]', ' ', text)
     text = re.sub(r'\s+', ' ', text)
@@ -44,8 +61,10 @@ def clean(text):
     # splits a sentence into words
     words = [nltk.word_tokenize(sent) for sent in sentences]
     # removes stop words
+    additional_stop_words = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'often', 'accross', 'knows', 'known', 'weeks', 'would', 'should', 'still', 'more', 'many', 'know', 'said', 'says', 'also', 'want', 'make', 'one', 'two', 'four', 'three', 'took', 'already', 'may', 'look', 'who', 'whose', 'get', 'take', 'like', 'including', 'include', 'more', 'much', 'first', 'week', 'day', 'recently','year','years']
+    stop_words = stopwords.words('english') + additional_stop_words
     for i in range(len(words)):
-        words[i] = [w for w in words[i] if w not in stopwords.words('english') and len(w) > 2]
+        words[i] = [w for w in words[i] if w not in stop_words and len(w) > 2]
     return  words
 
 def visualize_simiilarity(visualization_df):
@@ -60,8 +79,6 @@ def visualize_simiilarity(visualization_df):
     for idx, year in enumerate(df['year'].unique()):
         chart = px.bar(df[df['year']==year], x="word", y="score", color='year')
         chart.update_layout(title_text=str(year))
-        chart.update_layout(yaxis_range=[0.9, 1])
-        chart.update_layout(yaxis2 = dict(range=[0.9,1]))
         if idx < 3:
             fig.append_trace(chart.data[0], row=1, col=idx+1)
         else:
