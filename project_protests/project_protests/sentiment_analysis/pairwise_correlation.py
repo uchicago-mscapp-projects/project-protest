d625f417733cec560571e4b77cf2115b1c7574ef
Monica Nimmagadda <monican@linux6.cs.uchicago.edu>
2023-03-04 14:22:56 -0600

    merge?

--- project_protests/project_protests/sentiment_analysis/pairwise_correlation.py
+++ project_protests/project_protests/sentiment_analysis/pairwise_correlation.py
@@ -14,17 +14,23 @@ stemmer = PorterStemmer()
 
 def word_similarity(term):
     # upload text
-    df = pd.read_csv('/home/monican/capp30122/30122-project-project-protest/project_protests/clean_data/raw_data/nyt_articles.csv')
+    df = pd.read_csv('/home/monican/capp30122/30122-project-project-protest/project_protests/project_protests/api_requests/the_guardian/data/the_guardian_compiled.csv')
+    df_nyt = pd.read_csv('/home/monican/capp30122/30122-project-project-protest/project_protests/clean_data/raw_data/nyt_articles.csv')
     df['year'] = pd.DatetimeIndex(df['date']).year
-    years = df['year'].unique()
+    df_nyt['year'] = pd.DatetimeIndex(df_nyt['date']).year
+    df.merge(df_nyt[['year', 'lead_paragraph']])
 
+    years = sorted(df['year'].unique())
+    visualization_df = []
     for year in years:
         articles_text = df.loc[df['year']==year]['lead_paragraph'].str.cat(sep=' ')
         text = clean(articles_text)
-        if year == 2019: 
-            print(text)
-        # mode = statistics.mode(text[0])
-        word2vec = Word2Vec(text, min_count = 10, sg=1, window=7)
+        if year == 2017:
+            word2vec = Word2Vec(text, min_count = 1, sg=1, window=7)
+        elif year == 2023:
+            continue
+        else:
+            word2vec = Word2Vec(text, min_count = 10, sg=1, window=7)
         similar_words = word2vec.wv.most_similar(positive=[term], topn=10)
         visualize_simiilarity(similar_words, year)
     return None
@@ -42,11 +48,23 @@ def clean(text):
         words[i] = [w for w in words[i] if w not in stopwords.words('english') and len(w) > 2]
     return  words
 
-def visualize_simiilarity(similar_words, year):
-    title = str(year)
-    df = pd.DataFrame(similar_words, columns=["word", "score"])
-    hist = px.histogram(df, x="word", y="score",title=title)
-    hist.update_layout(yaxis_range=[0.9, 1.0])
-    # hist.update_layout(title_x: year)
-    hist.show()
+def visualize_simiilarity(visualization_df):
+    '''
+    This function takes in the similarity words list and creates bar charts
+    by year for each set of words and score. 
+    The resulting plot is a 2x3 chart of each year 2017-2022 and their corresponding
+    top words related to "police" as seen in NYT and the Guardian data.
+    '''
+    df = pd.DataFrame(visualization_df, columns=["word", "score", "year"])
+    fig = make_subplots(rows=2,cols=3)
+    for idx, year in enumerate(df['year'].unique()):
+        chart = px.bar(df[df['year']==year], x="word", y="score", color='year')
+        chart.update_layout(title_text=str(year))
+        chart.update_layout(yaxis_range=[0.9, 1])
+        chart.update_layout(yaxis2 = dict(range=[0.9,1]))
+        if idx < 3:
+            fig.append_trace(chart.data[0], row=1, col=idx+1)
+        else:
+            fig.append_trace(chart.data[0], row=2, col=(idx+1)-3)
+    fig.show()
     return None
