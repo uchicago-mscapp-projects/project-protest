b29ea5e6c032cb02a0e24ae7a9d3351caf506e9a
Josemaria Macedo Carrillo <josemaria@linux4.cs.uchicago.edu>
2023-03-06 14:17:47 -0600

    Changes to main file and clean_data module

--- project_protests/__main__.py
+++ project_protests/__main__.py
@@ -1,8 +1,7 @@
 import sys
 from project_protests.newspaper.compile_news_data import compile_news_data
 import os
-from  project_protests.html.dashboard import app 
-import subprocess
+from  project_protests.html.dashboard import app
 import random
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -20,26 +19,19 @@ if __name__ == "__main__":
                 default. If you wanna create the JSON files before compiling\
                 add 'collect_data' as the last argument.")
             compile_news_data()
-        elif sys.argv[1] == "dashboard":
-            print("Run dashboard")
-            app.run_server(port=port,debug = True)
-
         elif sys.argv[1] == "run":
-            print("Will run both 'compile_news' and 'dashboard' without\
+            print("Will run both 'dashboard' and 'compile_news' without\
                     creating the JSON files by default. If you wanna create the\
                     JSON files before compiling add 'collect_data' as the last\
                     argument.")
             compile_news_data()
             app.run_server(port=port,debug = True)
         else:
-            print("Incorrect arguments. Send 'compile_news', 'dashboard' or\
-                    'run'")
+            print("Incorrect arguments. Send 'compile_news' or 'run'")
     
     elif len(sys.argv) == 3:
         if sys.argv[1] == "compile_news" and sys.argv[2] == "collect_data":
             compile_news_data(True)
-        elif sys.argv[1] == "dashboard":
-            print("Too many arguments. Send just 'dashboard'")
         elif sys.argv[1] == "run" and sys.argv[2] == "collect_data":
             compile_news_data(collect_data = True)
             app.run_server(port=port,debug = True)
