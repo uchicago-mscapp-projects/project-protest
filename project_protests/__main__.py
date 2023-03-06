4a08c1d0aae47b6066bc099db70511fef23b64cc
Josemaria Macedo Carrillo <josemaria@linux4.cs.uchicago.edu>
2023-03-06 14:52:42 -0600

    change in main with prints

--- project_protests/__main__.py
+++ project_protests/__main__.py
@@ -10,24 +10,24 @@ port = random.randint(5000, 10000)
 if __name__ == "__main__":
 
     if len(sys.argv) == 1 :
-        print("Run dashboard")
+        print("Run dashboard.")
         app.run_server(port=port,debug = True)
 
     elif len(sys.argv) == 2:
         if sys.argv[1] == "compile_news":
             print("Will run 'compile_news' without creating the JSON files by\
                 default. If you wanna create the JSON files before compiling\
-                add 'collect_data' as the last argument.")
+                add 'collect_data' as the second argument.")
             compile_news_data()
         elif sys.argv[1] == "run":
             print("Will run both 'dashboard' and 'compile_news' without\
                     creating the JSON files by default. If you wanna create the\
-                    JSON files before compiling add 'collect_data' as the last\
-                    argument.")
+                    JSON files before compiling add 'collect_data' as the\
+                    second argument.")
             compile_news_data()
             app.run_server(port=port,debug = True)
         else:
-            print("Incorrect arguments. Send 'compile_news' or 'run'")
+            print("Incorrect arguments. Send 'compile_news' or 'run'.")
     
     elif len(sys.argv) == 3:
         if sys.argv[1] == "compile_news" and sys.argv[2] == "collect_data":
@@ -36,9 +36,9 @@ if __name__ == "__main__":
             compile_news_data(collect_data = True)
             app.run_server(port=port,debug = True)
         else:
-            print("Incorrect arguments. Send 'compile_news', 'dashboard' or\
-                    'run'")
+            print("Incorrect arguments. Send 'compile_news' or 'run' as first\
+                    argument and 'collect_data as second argument.")
 
     else:
-        print("Too many arguments. Send 'compile_news', 'dashboard', or\
+        print("Too many arguments. Sen no arguments, or send 'compile_news' or\
                 'run'")
