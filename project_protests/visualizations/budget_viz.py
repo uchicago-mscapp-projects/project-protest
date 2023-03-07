965fe4ef24fe0cc395ef9ba64d5a6697e4bc5d28
Lisette Solis <lisettesolis@linux7.cs.uchicago.edu>
2023-03-06 20:54:26 -0600

    Adding dropdown

--- project_protests/visualizations/budget_viz.py
+++ project_protests/visualizations/budget_viz.py
@@ -7,37 +7,57 @@ from .protest_viz import protest_data
 from project_protests.police_budget.budget_analysis import load_budget_data
 
 def budget_viz():
-    """"
     """
+    """
+    # create traces for figure 
+    traces = []
+    dropdowns = []
+    buttons = []
+
     df = load_budget_data()
     df.drop(df.index[(df["Type"] == "Total") | (df["Type"] == "Population")], inplace=True)
     df.drop(columns=["Type"], inplace=True)
     df = df.melt(id_vars="City", value_name="Total")
 
-    # Create figure with secondary y-axis
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
-
     # add traces for cities
     cities = list(df["City"].unique())
     for city in cities:
         sub_df = df.loc[df['City'] == city]
-        fig.add_trace(
-            go.Bar( 
+        trace = go.Bar( 
                 x=sub_df['variable'],
                 y=sub_df['Total'], 
-                name=city
-            ), secondary_y=False,
-        )
+                name=city)
+        traces.append(trace)
+        dropdowns.append({'label': city, 'value':trace})
+        
+    for i, dropdown in enumerate(dropdowns):
+        visible = [False] * len(dropdowns)
+        visible[i] = True
+        buttons.append({'args': [{'visible': visible}],
+                        'label': dropdowns[i]['label'], \
+                        'method': 'restyle'
+                        })
 
+    initial_layout = go.Layout(
+        updatemenus=[{'buttons': buttons,
+            'direction': 'down',
+            'showactive': True,
+            'x': 0.1,
+            'y': 1.2
+                }
+            ],
+        xaxis=dict(title='Year'),
+        yaxis=dict(title='Count'),
+        title="Per Capita Police Budget", title_x=0.5,
+        template="simple_white", 
+            )
 
-    fig.update_yaxes(title_text="Per Capita Budget ($)", secondary_y=False)
-    # fig.update_yaxes(title_text="Protests (#)", secondary_y=True)
-    fig.update_layout(template="simple_white", 
-        title="Per Capita Police Budget", title_x=0.5)
-    
+    #Create the figure with the traces and initial layout
+    fig = go.Figure(data=trace, layout=initial_layout)
+
     colors = ['#98A4D7', '#7BAE82', '#1e4477', '#F0CF56', '#9B5B41', '#3F367A', '#4D797D', \
         '#98A4D7', '#7BAE82', '#1e4477', '#F0CF56', '#9B5B41', '#3F367A', '#4D797D']
     for i,_ in enumerate(fig.data):
         fig.data[i].marker.color = colors[i]
-
+    
     return fig
